cc9a50f7e808068ced703a00595bb73fc92c8b6d
KNikooo <kissnikolettanett@gmail.com>
2022-03-23 11:43:19 +0100

    nyugta4: commitok eltavolitasa

--- src/nyugtaprojektek/Nyugta4.java
+++ src/nyugtaprojektek/Nyugta4.java
@@ -6,12 +6,9 @@ import java.util.function.BiConsumer;
 public class Nyugta4 {
 
     public static void main(String[] args) {
-//        String csillagok = "********************";
         disz("*");
-//        System.out.println(csillagok);
         System.out.printf("%14s\n", "Nyugta 3");
         disz("*");
-//        System.out.println(csillagok);
         
         final String HUF = "Ft";        
         int tetel1 = 350, tetel2 = 90, tetel3 = 1320;
@@ -19,20 +16,12 @@ public class Nyugta4 {
         System.out.printf("%10s: %5d %s\n", "Tétel 2", tetel2, HUF);
         System.out.printf("%10s: %5d %s\n", "Tétel 3", tetel3, HUF);
 
-//        for (int i = 1; i < 4; i++) {
-//            System.out.printf("Tétel%2d: %8d %s\n", i, tetel1, HUF);
-//        }
-
-        //String duplaVonal = "====================";
         disz("=");
-//        System.out.println(duplaVonal);
         
         int osszesen = tetel1 + tetel2 + tetel3;
         System.out.printf("%10s: %5d %s\n", "Összesen", osszesen, HUF);
         
-        //String szaggatottVonal = "--------------------";
         disz("-");
-//        System.out.println(szaggatottVonal);
         
         int szervizDijMertek = 10;
         int szervizDij = osszesen / szervizDijMertek;
@@ -40,35 +29,26 @@ public class Nyugta4 {
         System.out.printf("(%d%%)\n", szervizDijMertek);
         
         disz("=");
-//        System.out.println(duplaVonal);
 
         int fizetendo = osszesen + szervizDij;
         System.out.printf("%10s:  %d %s\n", "Fizetendő", fizetendo, HUF);
         double euro = fizetendo / 350.0;
         final String eur = "\u20ac";
         
-        System.out.printf("%10s%7.2f %s\n","", euro, eur);//
+        System.out.printf("%10s%7.2f %s\n","", euro, eur);
         
         disz("-");
-//        System.out.println(szaggatottVonal);
         
         System.out.println("");
-        //String rovidVonal = "_______";
         ketVonalValasztoval("_");
-//        System.out.print(rovidVonal);
-        String rovidVonalValaszto = "      ";
-//        System.out.print(rovidVonalValaszto);
-//        System.out.println(rovidVonal);
         System.out.print(" Dátum");
+        String rovidVonalValaszto = "      ";
         System.out.print(rovidVonalValaszto);
         System.out.println("   Név");
         
-        disz("*");
-//        System.out.println(csillagok);    
+        disz("*");  
         System.out.println("        CÉG");
         disz("*");
-//        System.out.println(csillagok);
-        
     }
 
     private static void disz(String jel) {
